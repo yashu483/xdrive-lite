66d2da27dd2f92ed600f3765dc0692aaa3b86e17
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2026-02-10 15:22:05 +0530

    add signup route handling functionality on backend use express-validator for validation and sanitization of user input and accordingly redirect user to next route

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,3 +1,5 @@
+import bcrypt from "bcryptjs";
+import { body, validationResult, matchedData } from "express-validator";
 import { prisma } from "./../lib/prisma.js";
 
 const indexGet = (req, res, next) => {
@@ -5,7 +7,7 @@ const indexGet = (req, res, next) => {
 };
 
 const loginGet = (req, res, next) => {
-  res.send("Login");
+  res.render("login");
 };
 
 const loginPost = (req, res, next) => {
@@ -13,11 +15,82 @@ const loginPost = (req, res, next) => {
 };
 
 const signupGet = (req, res, next) => {
-  res.send("Sign Up");
+  res.render("signup");
 };
 
-const signupPost = (req, res, next) => {
-  res.send("Signup post");
-};
+// validate sign up data
+const validateSignup = [
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Username cannot be empty")
+    .matches(/^[A-Za-z0-9._-]+$/)
+    .withMessage(
+      "username can only contain letters, numbers, underscore and dot.",
+    )
+    .isLength({ min: 3, max: 15 })
+    .withMessage("username should be between 3 and 15 characters only")
+    .custom(async (value) => {
+      const user = await prisma.user.findUnique({
+        where: {
+          username: value,
+        },
+      });
+      if (user) {
+        throw new Error("username already exists");
+      }
+      return true;
+    }),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("password cannot be empty")
+    .isStrongPassword({
+      minLength: 5,
+      minNumbers: 1,
+      minLowercase: 1,
+      minUppercase: 1,
+      minSymbols: 1,
+    })
+    .withMessage(
+      "Password should contain at least 1 uppercase,1 lowercase, 1 number, 1 symbol",
+    ),
+  body("confirmPass")
+    .trim()
+    .notEmpty()
+    .withMessage("password confirmation did not matched.")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("password confirmation did not matched.");
+      }
+      return true;
+    }),
+];
+
+const signupPost = [
+  validateSignup,
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.render("signup", { errors: errors.array() });
+        return;
+      }
+      const user = matchedData(req);
+      const hash = await bcrypt.hash(user.password, 10);
+
+      await prisma.user.create({
+        data: {
+          username: user.username,
+          password: hash,
+        },
+      });
+
+      res.redirect("/login");
+    } catch (err) {
+      next(err);
+    }
+  },
+];
 
 export default { indexGet, loginGet, loginPost, signupGet, signupPost };
