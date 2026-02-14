23a7fdca749bc99ecc07f697feef082bd78add17
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2026-02-14 09:54:44 +0530

    create and store new folders functionality user now are able to  create new folders and see them after successful creation

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,9 +1,137 @@
-const foldersGet = (req, res, next) => {
-  res.send("Folders");
+import { prisma } from "./../lib/prisma.js";
+import { body, validationResult, matchedData } from "express-validator";
+
+const foldersGet = async (req, res, next) => {
+  try {
+    if (!req.user) {
+      res.status(400).redirect("/");
+      return;
+    }
+
+    const errors = req.validationErr | null;
+    console.log();
+    const folderId =
+      typeof req.params.folderId === "string"
+        ? Number(req.params.folderId)
+        : null;
+
+    const files = await prisma.files.findMany({
+      where: {
+        folderId: folderId,
+      },
+    });
+    if (req.user && folderId) {
+      const folder = await prisma.folders.findFirst({
+        where: {
+          id: folderId,
+          userId: req.user.id,
+        },
+        include: {
+          children: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+          parent: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+          _count: {
+            select: {
+              files: true,
+            },
+          },
+        },
+      });
+      console.log(folder, typeof errors, null, undefined);
+      res.render("folders", { folder: folder, errors: errors });
+      return;
+    }
+    if (req.user && !folderId) {
+      const folders = await prisma.folders.findMany({
+        where: {
+          parentId: null,
+          userId: req.user.id,
+        },
+        select: {
+          name: true,
+          id: true,
+          _count: {
+            select: {
+              files: true,
+            },
+          },
+        },
+      });
+      console.log(folders, errors);
+      res.render("folders", { folders: folders, errors: errors });
+      return;
+    }
+  } catch (err) {
+    next(err);
+  }
 };
+
+const validateFolderName = [
+  body("folderName")
+    .trim()
+    .notEmpty()
+    .withMessage("Folder name cannot be empty")
+    .escape()
+    .custom(async (value, { req }) => {
+      const parentId =
+        typeof req.params.folderId === "string"
+          ? Number(req.params.folderId)
+          : null;
+      const folder = await prisma.folders.findFirst({
+        where: {
+          userId: req.user.id,
+          name: value,
+          parentId: req.params.folderId ? Number(req.params.folderId) : null,
+        },
+      });
+      if (folder) {
+        throw new Error("Folder name already exists.");
+      }
+      return true;
+    }),
+];
+const folderPost = [
+  validateFolderName,
+  async (req, res) => {
+    const errors = validationResult(req);
+    const parentId = req.params.folderId;
+
+    // checks for validation err
+    if (!errors.isEmpty()) {
+      res.locals.validationErr = errors.array();
+      parentId
+        ? res.redirect(`/folders/${parentId}`)
+        : res.redirect("/folders");
+      console.log(errors.array());
+      return;
+    }
+
+    // if validation succeed
+    const data = matchedData(req);
+    await prisma.folders.create({
+      data: {
+        name: data.folderName,
+        parentId: parentId ? Number(parentId) : null,
+        userId: req.user.id,
+      },
+    });
+    parentId ? res.redirect(`/folders/${parentId}`) : res.redirect("/folders");
+  },
+];
 
 const folderDelete = (req, res, next) => {
   res.send("Delete");
 };
-
-export default { foldersGet, folderDelete };
+const filesGet = async (req, res, next) => {};
+const filesPost = async (req, res, next) => {};
+const filesDelete = async (req, res, next) => {};
+export default { foldersGet, folderDelete, folderPost };
--- routes/foldersRouter.js
+++ routes/foldersRouter.js
@@ -3,5 +3,9 @@ import controller from "./../controllers/folderController.js";
 const folderRouter = Router();
 
 folderRouter.get("/", controller.foldersGet);
+folderRouter.get("/:folderId", controller.foldersGet);
+
+folderRouter.post("/", controller.folderPost);
+folderRouter.post("/:folderId", controller.folderPost);
 
 export default folderRouter;
