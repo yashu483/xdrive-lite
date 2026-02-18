f9c138df576fbe5494765b8764f4a626ed04fa94
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2026-02-18 13:31:05 +0530

    feat: Add bottomSheet toggle form to create folder Create a bottom sheet toggle form for smartphone size screens to create new folders.

---
+++ public/scripts/forms-toggle.js
@@ -0,0 +1,30 @@
+const overlay = document.getElementById("overlay");
+const bottomSheet = document.getElementById("bottomSheet");
+const input = document.getElementById("folderName");
+const createBtn = document.getElementById("createBtn");
+
+function openModal() {
+  overlay.classList.add("active");
+  bottomSheet.classList.add("active");
+}
+
+function closeModal() {
+  overlay.classList.remove("active");
+  bottomSheet.classList.remove("active");
+  bottomSheet.addEventListener("submit", (e) => {
+    bottomSheet.requestSubmit();
+  });
+}
+// createBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   bottomSheet.requestSubmit();
+// });
+function handleInput() {
+  if (input.value.trim().length > 0) {
+    createBtn.disabled = false;
+    createBtn.classList.add("enabled");
+  } else {
+    createBtn.disabled = true;
+    createBtn.classList.remove("enabled");
+  }
+}
